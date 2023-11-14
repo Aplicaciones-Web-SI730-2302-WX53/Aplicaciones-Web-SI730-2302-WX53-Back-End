942e4e2082f9d339e34dce459399d2084a1b15b3
Naldo Reupo <109175146+Naldo-reupo-LIM@users.noreply.github.com>
2023-11-13 21:49:01 -0500

    fix

--- C Data/Model/Base.cs
+++ C Data/Model/Base.cs
@@ -2,7 +2,7 @@ namespace Data;
 
 public class Base
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     public DateTime DateCreated { get; set; }
     public DateTime? DateUdpate { get; set; }
     public bool IsActive { get; set; }
